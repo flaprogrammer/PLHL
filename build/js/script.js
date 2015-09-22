27b0406ba9d0d357e4ce45ee11947858c35d72f4
lenar <lenar@smarthead.ru>
2015-09-22 18:41:32 +0300

    + team.html

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,36 @@
+var i, j, k, lastnums, len, ref, ref1;
+
+if ($('.teamplayers__player').length) {
+  len = $('.teamplayers__player').length;
+  lastnums = len % 4 || 4;
+  for (i = j = 1, ref = lastnums; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+    $('.teamplayers__player').eq(-i).addClass('teamplayers__player--lastrow');
+  }
+}
+
+if ($('.plyrs__el').length) {
+  len = $('.plyrs__el').length;
+  lastnums = len % 2 || 2;
+  for (i = k = 1, ref1 = lastnums; 1 <= ref1 ? k <= ref1 : k >= ref1; i = 1 <= ref1 ? ++k : --k) {
+    $('.plyrs__el').eq(-i).addClass('plyrs__el--lastrow');
+  }
+}
+
+if ($('.teamplayers__season').length) {
+  $('.teamplayers__season').customSelect();
+}
+
+if ($('.teamplayers__tab').length) {
+  $('.teamplayers__tab').on('click', function() {
+    var index;
+    index = $('.teamplayers__tab').index($(this));
+    $('.teamplayers__tab').removeClass('teamplayers__tab--active');
+    $(this).addClass('teamplayers__tab--active');
+    $('.teamplayers__content').removeClass('teamplayers__content--active');
+    return $('.teamplayers__content').eq(index).addClass('teamplayers__content--active');
+  });
+}
+
 var Mygallery, g1;
 
 Mygallery = function(opts) {
---
+++ src/js/script/calculus.coffee
@@ -0,0 +1,27 @@
+if $('.teamplayers__player').length
+	len = $('.teamplayers__player').length
+	lastnums = len % 4 || 4
+	for i in [1..lastnums]
+		$('.teamplayers__player').eq(-i).addClass('teamplayers__player--lastrow')
+
+if $('.plyrs__el').length
+	len = $('.plyrs__el').length
+	lastnums = len % 2 || 2
+	for i in [1..lastnums]
+		$('.plyrs__el').eq(-i).addClass('plyrs__el--lastrow')
+
+if $('.teamplayers__season').length
+	$('.teamplayers__season').customSelect()
+
+if $('.teamplayers__tab').length
+	$('.teamplayers__tab').on('click', ->
+		index = $('.teamplayers__tab').index $(this)
+
+		$('.teamplayers__tab').removeClass('teamplayers__tab--active')
+		$(this).addClass('teamplayers__tab--active')
+
+		$('.teamplayers__content').removeClass('teamplayers__content--active')
+		$('.teamplayers__content').eq(index).addClass('teamplayers__content--active')
+
+
+	)

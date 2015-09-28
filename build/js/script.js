2d017510d8b018cc286ea595273def28ed849c97
lenar <lenar@smarthead.ru>
2015-09-28 15:01:35 +0300

    fix галерея

--- build/js/script.js
+++ build/js/script.js
@@ -17,7 +17,7 @@ if ($('.teamplayers__tab').length) {
   });
 }
 
-var Mygallery, SimpleGallery, g1, g2;
+var Mygallery, TwoGallery, g1, g2, g3, gal_index;
 
 Mygallery = function(opts) {
   var copy, copy2, self;
@@ -92,9 +92,63 @@ Mygallery.prototype.showSlide = function(val) {
   });
 };
 
-SimpleGallery = function() {};
+TwoGallery = function(opts) {
+  var self;
+  opts = opts || {};
+  opts.selector = opts.selector || '';
+  opts.slide = opts.slide || '.img';
+  opts.prevBut = opts.prevBut || '.teams__leftbut';
+  opts.nextBut = opts.nextBut || '.teams__rightbut';
+  this.$slides = $(opts.selector);
+  opts.width = opts.width || this.$slides.width();
+  this.true_width = this.$slides.width();
+  this.slide_width = 300;
+  this.playing = false;
+  self = this;
+  this.slide_width = opts.width;
+  this.len = Math.ceil(this.true_width / this.slide_width);
+  this.$slides.css('left', 0 + 'px');
+  this.current = 0;
+  $(opts.prevBut).on('click', function() {
+    return self.goToSlide('prev');
+  });
+  $(opts.nextBut).on('click', function() {
+    return self.goToSlide('next');
+  });
+  return this;
+};
 
-SimpleGallery.prototype = new Mygallery();
+TwoGallery.prototype.goToSlide = function(val) {
+  var v;
+  if (this.playing) {
+    return;
+  }
+  if (val === 'prev') {
+    v = -1;
+  }
+  if (val === 'next') {
+    v = 1;
+  }
+  this.current += v;
+  if (this.current < 0) {
+    this.current = this.len - 1;
+  }
+  if (this.current > this.len - 1) {
+    this.current = 0;
+  }
+  return this.showSlide(this.current);
+};
+
+TwoGallery.prototype.showSlide = function(val) {
+  var self;
+  self = this;
+  this.playing = true;
+  return this.$slides.velocity({
+    left: -val * self.slide_width
+  }, 800, function() {
+    return self.playing = false;
+  });
+};
 
 g1 = new Mygallery({
   selector: '.teams__images',
@@ -104,10 +158,34 @@ g1 = new Mygallery({
   width: 300
 });
 
-g2 = new SimpleGallery({
+g2 = new TwoGallery({
   selector: '.results__list',
   slide: '.results__el',
   prevBut: '.results__leftbut',
   nextBut: '.results__rightbut',
   width: 660
+});
+
+gal_index = 0;
+
+$('.gal__el').each(function(index, el) {
+  if ($(el).hasClass('gal__el--big')) {
+    if (gal_index % 2 === 1) {
+      $(el).css('margin-left', '180px');
+    }
+    return gal_index = 0;
+  } else if ($(el).hasClass('gal__el--small')) {
+    if (gal_index % 2 === 0) {
+      $(el).css('position', 'absolute');
+    }
+    return gal_index++;
+  }
+});
+
+g3 = new TwoGallery({
+  selector: '.gal__list',
+  slide: '.gal__el',
+  prevBut: '.gal__leftbut',
+  nextBut: '.gal__rightbut',
+  width: 650
 });
--- src/js/script/gallery.coffee
+++ src/js/script/gallery.coffee
@@ -68,9 +68,60 @@ Mygallery.prototype.showSlide = (val) ->
 	)
 
 
-SimpleGallery = ->
 
-SimpleGallery.prototype = new Mygallery()
+TwoGallery = (opts) ->
+	opts = opts || {}
+	opts.selector = opts.selector || ''
+	opts.slide = opts.slide || '.img'
+	opts.prevBut = opts.prevBut || '.teams__leftbut'
+	opts.nextBut = opts.nextBut || '.teams__rightbut'
+
+	this.$slides = $(opts.selector)
+	opts.width = opts.width || this.$slides.width()
+
+	this.true_width = this.$slides.width()
+
+	this.slide_width = 300
+	this.playing = false
+	self = this
+
+
+	this.slide_width = opts.width
+
+	this.len = Math.ceil(this.true_width / this.slide_width)
+	this.$slides.css('left', 0+'px')
+	this.current = 0
+
+
+	$(opts.prevBut).on('click', -> self.goToSlide('prev'))
+	$(opts.nextBut).on('click', -> self.goToSlide('next'))
+
+	return this
+
+
+TwoGallery.prototype.goToSlide = (val) ->
+
+	return if this.playing
+	v = -1 if val=='prev'
+	v = 1 if val=='next'
+
+	this.current += v
+	this.current = this.len-1 if this.current<0
+	this.current = 0 if this.current>this.len-1
+
+	this.showSlide(this.current)
+
+
+TwoGallery.prototype.showSlide = (val) ->
+	self = this
+	this.playing = true
+	this.$slides.velocity({
+			left: -val*self.slide_width
+		}, 800, ->
+		self.playing = false
+	)
+
+
 
 
 
@@ -82,11 +133,33 @@ g1 = new Mygallery({
 	width: 300
 })
 
-g2 = new SimpleGallery({
+g2 = new TwoGallery({
 	selector : '.results__list'
 	slide : '.results__el'
 	prevBut : '.results__leftbut'
 	nextBut : '.results__rightbut'
 	width: 660
+})
+
+gal_index = 0
+$('.gal__el').each((index, el) ->
+	#$(el).css('position', 'absolute') if index % 2 == 0
+	if $(el).hasClass('gal__el--big')
+		$(el).css('margin-left', '180px') if gal_index%2 == 1
+		gal_index = 0
+	else if $(el).hasClass('gal__el--small')
+		$(el).css('position', 'absolute') if gal_index%2 == 0
+		gal_index++
+
+
+)
+
+
+g3 = new TwoGallery({
+	selector : '.gal__list'
+	slide : '.gal__el'
+	prevBut : '.gal__leftbut'
+	nextBut : '.gal__rightbut'
+	width: 650
 })
 
